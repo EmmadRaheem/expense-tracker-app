0e451a158d95b84dd22dc7b85d75f1dcfdb3dd22
EmmadRaheem <EmmadRaheem@users.noreply.github.com>
2020-06-20 19:19:53 +0500

    Update Head.js

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Head = () => {
     return (
         <div>
-            <h2>Expense Tracker</h2>
+            <h2>Expense Tracker by Emmad Raheem</h2>
         </div>
     )
 }
